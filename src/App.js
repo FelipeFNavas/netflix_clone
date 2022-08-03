848f022654a91d32e80cdce95b48dd9b64f9b982
Felipe Navas <felipefnavas@gmail.com>
2022-08-03 16:40:37 -0300

    fix: solving dimension problems

--- src/App.js
+++ src/App.js
@@ -65,6 +65,15 @@ export default () => {
         <br />
         Dados pegos do site Themoviedb.org
       </footer>
+
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img
+            src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
+            alt="Carregando"
+          />
+        </div>
+      )}
     </div>
   );
 };
